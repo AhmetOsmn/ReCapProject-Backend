0df57c08616f0fc20ca41c80fca00da25823f0a9
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-02-03 00:28:41 +0300

    Business kisminda Interface Duzenlendi

--- Business/Abstract/ICarRentalService.cs
+++ Business/Abstract/ICarRentalService.cs
@@ -7,5 +7,9 @@ namespace Business.Abstract
     public interface ICarRentalService<T>
     {
         List<T> GetAll();
+        T GetById(int id);
+        void Add(T t);
+        void Delete(T t);
+        void Update(T t);
     }
 }
