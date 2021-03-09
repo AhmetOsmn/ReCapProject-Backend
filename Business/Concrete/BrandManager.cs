c528dc320677283423e19f14df6deca959f2e10a
Ahmet Osman Sezgin <180401022@ogr.comu.edu.tr>
2021-03-09 12:25:57 +0300

    Cache, Transaction ve Performance aspectleri eklendi.

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspects.Autofac;
 using Business.Contants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -23,19 +24,22 @@ namespace Business.Concrete
             _brandDal = brandDal;
         }
 
+        [CacheAspect]
         public IDataResult<List<Brand>> GetAll()
         {
             return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(), Messages.BrandsListed);
         }
 
+        [CacheAspect]
+        //[PerformanceAspect(5)]
         public IDataResult<Brand> GetById(int id)
         {
             return new SuccessDataResult<Brand>(_brandDal.Get(g => g.BrandId == id));
         }
 
+        [SecuredOperation("brand.add,admin")]
         [ValidationAspect(typeof(BrandValidator))]
-        [SecuredOperation("productçadd,admin")]
-
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Brand brand)
         {
             _brandDal.Add(brand);
@@ -46,10 +50,26 @@ namespace Business.Concrete
             _brandDal.Delete(brand);
             return new Result(true, Messages.BrandDeleted);
         }
+
+        [ValidationAspect(typeof(BrandValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Brand brand)
         {
             _brandDal.Update(brand);
             return new Result(true, Messages.BrandUpdated);
+        }
+
+        public IResult AddTransactionalTest(Brand brand)
+        {
+            Add(brand);
+
+            if(brand.Name.Length < 2)
+            {
+                throw new Exception("");
+            }
+            Add(brand);
+
+            return null;
         }
     }
 }
